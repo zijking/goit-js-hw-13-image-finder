abe08097acea9c7181d07d482ee78093a398fa76
Fedir <zi.12feodor@gmail.com>
2020-11-13 21:33:17 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
-import './styles.css';
+import './sass/main.scss';
 import './js/apiService';
 
-import searchApi from './js/apiService';
+import SearchApi from './js/apiService';
+import imgCardTem from './templetes/img-card.hbs';
+import debounce from 'lodash.debounce';
 
-const res = searchApi('dog');
+const searchApi = new SearchApi();
+
+const listEl = document.querySelector('.js-gallery');
+const formEl = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.js-load-more');
+
+formEl.addEventListener('input', debounce(searchImg, 500));
+loadMoreBtn.addEventListener('click', loadMoreImg);
+
+function renderListImg(listImg) {
+  listEl.insertAdjacentHTML('beforeend', imgCardTem(listImg));
+}
+
+async function searchImg(e) {
+  resetGalery();
+  searchApi.query = formEl.elements.query.value;
+  const rez = await searchApi.searchImg();
+
+  renderListImg(rez.hits);
+}
+
+async function loadMoreImg() {
+  searchApi.incrementPage();
+  const rez = await searchApi.searchImg();
+  renderListImg(rez.hits);
+
+  // console.log(document.body.scrollHeight);
+  // window.scrollTo(0, );
+  setTimeout(() => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      left: 0,
+      behavior: 'smooth',
+    });
+    // console.log('scroll');
+  }, 700);
+}
+
+function resetGalery() {
+  // formEl.elements.query.value = '';
+  listEl.innerHTML = '';
+  searchApi.resetPage();
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,10 +1,34 @@
-console.log('apiService.js');
+// console.log('apiService.js');
 
 const API_KEY = '19017356-b8756222892cfba3d959da30c';
 const BASE_URL = 'https://pixabay.com/api/';
+const ON_PAGE = 12;
 
-export default function searchImg(searchWord) {
-  return fetch(`${BASE_URL}\?key=${API_KEY}&q=${searchWord}`).then(response =>
-    response.json(),
-  );
+export default class SearchImgAi {
+  constructor() {
+    this.page = 1;
+    this.searchWord = '';
+  }
+
+  searchImg() {
+    return fetch(
+      `${BASE_URL}\?key=${API_KEY}&q=${this.searchWord}&per_page=${ON_PAGE}&page=${this.page}`,
+    ).then(response => response.json());
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchWord;
+  }
+
+  set query(newWord) {
+    this.searchWord = newWord;
+  }
 }
